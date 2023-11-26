690ddf90003474af6484e90b6c478d42346986df
VladimirCech <vladda.cech@seznam.cz>
2023-11-26 21:28:48 +0100

    Started with DE Rand/1/bin

--- benchmark_functions.py
+++ benchmark_functions.py
@@ -35,10 +35,10 @@ def griewank(x):
 # 6. Levy
 def levy(x):
     w = [(1 + (i - 1) / 4) for i in x]
-    part1 = np.sin(np.pi * w[0])**2
-    part2 = sum((w[i] - 1)**2 * (1 + 10 * np.sin(np.pi * w[i] + 1)**2)
+    part1 = np.sin(np.pi * w[0]) ** 2
+    part2 = sum((w[i] - 1) ** 2 * (1 + 10 * np.sin(np.pi * w[i] + 1) ** 2)
                 for i in range(len(w) - 1))
-    part3 = (w[-1] - 1)**2 * (1 + np.sin(2 * np.pi * w[-1])**2)
+    part3 = (w[-1] - 1) ** 2 * (1 + np.sin(2 * np.pi * w[-1]) ** 2)
     return part1 + part2 + part3
 
 
@@ -49,34 +49,34 @@ def schwefel(x):
 
 # 8. Zakharov
 def zakharov(x):
-    part1 = sum(i**2 for i in x)
+    part1 = sum(i ** 2 for i in x)
     part2 = sum(0.5 * i * x[i] for i in range(len(x)))
-    return part1 + part2**2 + part2**4
+    return part1 + part2 ** 2 + part2 ** 4
 
 
 # 9. Styblinski-Tang
 def styblinski_tang(x):
-    return 0.5 * sum(i**4 - 16*i**2 + 5*i for i in x)
+    return 0.5 * sum(i ** 4 - 16 * i ** 2 + 5 * i for i in x)
 
 
 # 10. Matyas
 def matyas_general(x):
-    sum_squares = sum(i**2 for i in x)
-    sum_products = sum(x[i] * x[i+1] for i in range(len(x) - 1))
+    sum_squares = sum(i ** 2 for i in x)
+    sum_products = sum(x[i] * x[i + 1] for i in range(len(x) - 1))
     return 0.26 * sum_squares - 0.48 * sum_products
 
 
 # 11. Booth
 def booth_general(x):
-    part1 = sum((x[i] + 2*x[i+1] - 7)**2 for i in range(len(x) - 1))
-    part2 = sum((2*x[i] + x[i+1] - 5)**2 for i in range(len(x) - 1))
+    part1 = sum((x[i] + 2 * x[i + 1] - 7) ** 2 for i in range(len(x) - 1))
+    part2 = sum((2 * x[i] + x[i + 1] - 5) ** 2 for i in range(len(x) - 1))
     return part1 + part2
 
 
 # 12. Three-hump camel
 def three_hump_camel_general(x):
-    sum_terms = sum(2*x[i]**2 - 1.05*x[i]**4 + x[i]**6 / 6 +
-                    x[i] * x[i+1] + x[i+1]**2 for i in range(len(x) - 1))
+    sum_terms = sum(2 * x[i] ** 2 - 1.05 * x[i] ** 4 + x[i] ** 6 / 6 +
+                    x[i] * x[i + 1] + x[i + 1] ** 2 for i in range(len(x) - 1))
     return sum_terms
 
 
@@ -84,8 +84,8 @@ def three_hump_camel_general(x):
 def easom_general(x):
     product_terms = 1
     for i in range(len(x) - 1):
-        product_terms *= -np.cos(x[i]) * np.cos(x[i+1]) * \
-            np.exp(-((x[i] - np.pi)**2 + (x[i+1] - np.pi)**2))
+        product_terms *= -np.cos(x[i]) * np.cos(x[i + 1]) * \
+                         np.exp(-((x[i] - np.pi) ** 2 + (x[i + 1] - np.pi) ** 2))
     return product_terms
 
 
@@ -93,70 +93,78 @@ def easom_general(x):
 def cross_in_tray_general(x):
     product_terms = 1
     for i in range(len(x) - 1):
-        product_terms *= -0.0001 * (abs(np.sin(x[i]) * np.sin(x[i+1]) * np.exp(
-            abs(100 - np.sqrt(x[i]**2 + x[i+1]**2) / np.pi)) + 1)**0.1)
+        product_terms *= -0.0001 * (abs(np.sin(x[i]) * np.sin(x[i + 1]) * np.exp(
+            abs(100 - np.sqrt(x[i] ** 2 + x[i + 1] ** 2) / np.pi)) + 1) ** 0.1)
     return product_terms
 
 
 # 15. Six-hump camel
 def six_hump_camel_back_general(x):
-    sum_terms = sum((4 - 2.1*x[i]**2 + x[i]**4 / 3)*x[i]**2 + x[i] *
-                    x[i+1] + (4*x[i+1]**2 - 4)*x[i+1]**2 for i in range(len(x) - 1))
+    sum_terms = sum((4 - 2.1 * x[i] ** 2 + x[i] ** 4 / 3) * x[i] ** 2 + x[i] *
+                    x[i + 1] + (4 * x[i + 1] ** 2 - 4) * x[i + 1] ** 2 for i in range(len(x) - 1))
     return sum_terms
 
 
 # 16. Drop-wave
 def drop_wave_general(x):
-    sum_terms = sum(- (1 + np.cos(12 * np.sqrt(x[i]**2 + x[i+1]**2))) / (
-        0.5 * (x[i]**2 + x[i+1]**2) + 2) for i in range(len(x) - 1))
+    sum_terms = sum(- (1 + np.cos(12 * np.sqrt(x[i] ** 2 + x[i + 1] ** 2))) / (
+            0.5 * (x[i] ** 2 + x[i + 1] ** 2) + 2) for i in range(len(x) - 1))
     return sum_terms
 
 
 # 17. Bukin_n6
 def bukin_n6_general(x):
-    sum_terms = sum(100 * np.sqrt(abs(x[i+1] - 0.01*x[i]**2)) +
+    sum_terms = sum(100 * np.sqrt(abs(x[i + 1] - 0.01 * x[i] ** 2)) +
                     0.01 * abs(x[i] + 10) for i in range(len(x) - 1))
     return sum_terms
 
 
 # 18. Goldstein-price
 def goldstein_price_general(x):
-    sum_terms = sum((1 + (x[i] + x[i+1] + 1)**2 * (19 - 14*x[i] + 3*x[i]**2 - 14*x[i+1] + 6*x[i]*x[i+1] + 3*x[i+1]**2)) * (30 + (
-        2*x[i] - 3*x[i+1])**2 * (18 - 32*x[i] + 12*x[i]**2 + 48*x[i+1] - 36*x[i]*x[i+1] + 27*x[i+1]**2)) for i in range(len(x) - 1))
+    sum_terms = sum((1 + (x[i] + x[i + 1] + 1) ** 2 * (
+            19 - 14 * x[i] + 3 * x[i] ** 2 - 14 * x[i + 1] + 6 * x[i] * x[i + 1] + 3 * x[i + 1] ** 2)) * (30 + (
+            2 * x[i] - 3 * x[i + 1]) ** 2 * (18 - 32 * x[i] + 12 * x[i] ** 2 + 48 * x[i + 1] - 36 * x[i] * x[
+        i + 1] + 27 * x[i + 1] ** 2)) for i in range(len(x) - 1))
     return sum_terms
 
 
 # 19. De Jong f4
 def de_jong_f4(x):
-    return sum((i + 1) * x_i**4 for i, x_i in enumerate(x))
+    return sum((i + 1) * x_i ** 4 for i, x_i in enumerate(x))
 
 
 # 20. Schaffer n4
 def schaffer_n4(x):
-    return sum(0.5 + (np.sin(np.sqrt(x[i]**2 + x[i+1]**2))**2 - 0.5) / (1 + 0.001 * (x[i]**2 + x[i+1]**2))**2 for i in range(len(x) - 1))
+    return sum(
+        0.5 + (np.sin(np.sqrt(x[i] ** 2 + x[i + 1] ** 2)) ** 2 - 0.5) / (1 + 0.001 * (x[i] ** 2 + x[i + 1] ** 2)) ** 2
+        for i in range(len(x) - 1))
 
 
 # 21. Salomon
 def salomon(x):
-    r = np.sqrt(sum(i**2 for i in x))
+    r = np.sqrt(sum(i ** 2 for i in x))
     return 1 - np.cos(2 * np.pi * r) + 0.1 * r
 
 
 # 22. Schaffer n2
 def schaffer_n2(x):
-    return sum(0.5 + (np.sin(x[i]**2 - x[i+1]**2)**2 - 0.5) / (1 + 0.001 * (x[i]**2 - x[i+1]**2))**2 for i in range(len(x) - 1))
+    return sum(
+        0.5 + (np.sin(x[i] ** 2 - x[i + 1] ** 2) ** 2 - 0.5) / (1 + 0.001 * (x[i] ** 2 - x[i + 1] ** 2)) ** 2 for i in
+        range(len(x) - 1))
 
 
 # 23. Sine-Envelope Sine-Wave
 def sine_envelope_sine_wave(x):
-    return sum(0.5 + (np.sin(np.sqrt(x[i]**2 + x[i+1]**2))**2 - 0.5) / (1 + 0.001 * (x[i]**2 + x[i+1]**2))**2 for i in range(len(x) - 1))
+    return sum(
+        0.5 + (np.sin(np.sqrt(x[i] ** 2 + x[i + 1] ** 2)) ** 2 - 0.5) / (1 + 0.001 * (x[i] ** 2 + x[i + 1] ** 2)) ** 2
+        for i in range(len(x) - 1))
 
 
 # 24. Modified-Exponential
 def modified_exponential(x):
-    return -np.exp(-0.5 * sum(i**2 for i in x))
+    return -np.exp(-0.5 * sum(i ** 2 for i in x))
 
 
 # 25. Modofied Bent Cigar
 def modified_bent_cigar(x):
-    return x[0]**2 + 10**6 * sum(i**2 for i in x[1:])
+    return x[0] ** 2 + 10 ** 6 * sum(i ** 2 for i in x[1:])
--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 from benchmark_functions import *
 
-x=[1,2,5,10]
 
-print(rastrigin(x), rastrigin.__name__)
-print(ackley(x))
-print(dejong(x))
-# print(goldstein_price(x))
-print(schaffer(x))
-print(griewank(x))
-print(michalewicz(x))
-# print(six_hump_camelback(x))
-# print(easom(x))
+def generate_population(count, dimension):
+    population = []
+    for i in range(count):
+        population.append(list(np.random.randint(
+            low=-100, high=101, size=dimension)))
+    return population
+
+
+# def de_rand_1_bin(dimensions, population_size, repetitions):
+
+def de_rand_1_bin(func, dim, pop_size, F=0.8, CR=0.9):
+
+    population = generate_population(pop_size, dim)
+    max_gen = dim*2000
+
+    # Hlavní smyčka algoritmu
+    for gen in range(max_gen):
+        for i in range(len(population)):
+            # Výběr tří různých indexů (r1, r2, r3) náhodně z populace
+            candidates = list(range(0, i)) + list(range(i + 1, pop_size))
+            r1, r2, r3 = population[np.random.choice(
+                candidates, 3, replace=False)]
+
+            # Mutace
+            mutant = r1 + F * (r2 - r3)
+            mutant = np.clip(mutant, 0, 1)  # Omezení mutantů v rozsahu [0, 1]
+
+            # Binární křížení
+            trial = np.copy(population[i])
+            for j in range(len(bounds)):
+                if np.random.rand() < CR:
+                    trial[j] = mutant[j]
+                # Kontrola hranic a upravení metodou odrazu
+                if trial[j] < bounds[j][0] or trial[j] > bounds[j][1]:
+                    trial[j] = bounds[j][0] + \
+                        abs(trial[j] - bounds[j][0]
+                            ) % (bounds[j][1] - bounds[j][0])
+
+            # Selekcí
+            if func(trial) < func(population[i]):
+                population[i] = trial
+
+    # Nalezení a vrácení nejlepšího řešení
+    best_idx = np.argmin([func(ind) for ind in population])
+    return population[best_idx]
