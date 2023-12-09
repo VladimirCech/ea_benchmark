3c9c9c6c04f182983718d22f837b6edb16811236
VladimirCech <vladda.cech@seznam.cz>
2023-12-09 16:35:17 +0100

    Made some modifications to benchmark functions, but they still need some more testing

--- benchmark_functions.py
+++ benchmark_functions.py
@@ -1,21 +1,26 @@
 import numpy as np
+from numpy import abs, cos, exp, mean, pi, prod, sin, sqrt, sum
 
 
 # 1. Sphere
 def sphere(x):
-    return sum(i ** 2 for i in x)
+    return sum(i**2 for i in x)
 
 
 # 2. Rastrigin
 def rastrigin(x):
-    return 10 * len(x) + sum(i ** 2 - 10 * np.cos(2 * np.pi * i) for i in x)
+    return 10 * len(x) + sum(i**2 - 10 * np.cos(2 * np.pi * i) for i in x)
 
 
 # 3. Ackley
 def ackley(x):
     n = len(x)
-    return -20 * np.exp(-0.2 * np.sqrt(sum(i ** 2 for i in x) / n)) - np.exp(
-        sum(np.cos(2 * np.pi * i) for i in x) / n) + 20 + np.e
+    return (
+        -20 * np.exp(-0.2 * np.sqrt(sum(i**2 for i in x) / n))
+        - np.exp(sum(np.cos(2 * np.pi * i) for i in x) / n)
+        + 20
+        + np.e
+    )
 
 
 # 4. Rosenbrock
@@ -25,7 +30,7 @@ def rosenbrock(x):
 
 # 5. Grienwank
 def griewank(x):
-    part1 = sum(i ** 2 / 4000 for i in x)
+    part1 = sum(i**2 / 4000 for i in x)
     part2 = 1
     for i, val in enumerate(x):
         part2 *= np.cos(val / np.sqrt(i + 1))
@@ -36,8 +41,7 @@ def griewank(x):
 def levy(x):
     w = [(1 + (i - 1) / 4) for i in x]
     part1 = np.sin(np.pi * w[0]) ** 2
-    part2 = sum((w[i] - 1) ** 2 * (1 + 10 * np.sin(np.pi * w[i] + 1) ** 2)
-                for i in range(len(w) - 1))
+    part2 = sum((w[i] - 1) ** 2 * (1 + 10 * np.sin(np.pi * w[i] + 1) ** 2) for i in range(len(w) - 1))
     part3 = (w[-1] - 1) ** 2 * (1 + np.sin(2 * np.pi * w[-1]) ** 2)
     return part1 + part2 + part3
 
@@ -49,21 +53,25 @@ def schwefel(x):
 
 # 8. Zakharov
 def zakharov(x):
-    part1 = sum(i ** 2 for i in x)
+    part1 = sum(i**2 for i in x)
     part2 = sum(0.5 * i * x[i] for i in range(len(x)))
-    return part1 + part2 ** 2 + part2 ** 4
+    return part1 + part2**2 + part2**4
 
 
 # 9. Styblinski-Tang
 def styblinski_tang(x):
-    return 0.5 * sum(i ** 4 - 16 * i ** 2 + 5 * i for i in x)
+    return 0.5 * sum(i**4 - 16 * i**2 + 5 * i for i in x)
 
 
-# 10. Matyas #####################################
-def matyas_general(x):
-    sum_squares = sum(i ** 2 for i in x)
-    sum_products = sum(x[i] * x[i + 1] for i in range(len(x) - 1))
-    return 0.26 * sum_squares - 0.48 * sum_products
+# 10. Power-Sum ###
+def powersum(x, b=[8, 18, 44, 114]):  # power.m
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+    s = 0
+    for k in range(1, n + 1):
+        bk = b[min(k - 1, len(b) - 1)]  # ?
+        s += (sum(x**k) - bk) ** 2  # dim 10 huge, 100 overflows
+    return s
 
 
 # 11. Booth
@@ -75,96 +83,105 @@ def booth_general(x):
 
 # 12. Three-hump camel
 def three_hump_camel_general(x):
-    sum_terms = sum(2 * x[i] ** 2 - 1.05 * x[i] ** 4 + x[i] ** 6 / 6 +
-                    x[i] * x[i + 1] + x[i + 1] ** 2 for i in range(len(x) - 1))
+    sum_terms = sum(
+        2 * x[i] ** 2 - 1.05 * x[i] ** 4 + x[i] ** 6 / 6 + x[i] * x[i + 1] + x[i + 1] ** 2 for i in range(len(x) - 1)
+    )
     return sum_terms
 
 
-# 13. Easom ??????????
-def easom_general(x):
-    product_terms = 1
-    for i in range(len(x) - 1):
-        product_terms *= -np.cos(x[i]) * np.cos(x[i + 1]) * \
-                         np.exp(-((x[i] - np.pi) ** 2 + (x[i + 1] - np.pi) ** 2))
-    return product_terms
+# 13. Sum-Squares
+def sum2(x):
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+    j = np.arange(1.0, n + 1)
+    return sum(j * x**2)
 
 
-# 14. Cross-in-tray ######################
-def cross_in_tray_general(x):
-    product_terms = 1
-    for i in range(len(x) - 1):
-        product_terms *= -0.0001 * (abs(np.sin(x[i]) * np.sin(x[i + 1]) * np.exp(
-            abs(100 - np.sqrt(x[i] ** 2 + x[i + 1] ** 2) / np.pi)) + 1) ** 0.1)
-    return product_terms
+# 14. Powell
+def powell(x):
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+    n4 = ((n + 3) // 4) * 4
+    if n < n4:
+        x = np.append(x, np.zeros(n4 - n))
+    x = x.reshape((4, -1))  # 4 rows: x[4i-3] [4i-2] [4i-1] [4i]
+    f = np.empty_like(x)
+    f[0] = x[0] + 10 * x[1]
+    f[1] = sqrt(5) * (x[2] - x[3])
+    f[2] = (x[1] - 2 * x[2]) ** 2
+    f[3] = sqrt(10) * (x[0] - x[3]) ** 2
+    return sum(f**2)
 
 
-# 15. Six-hump camel ????????
-def six_hump_camel_back_general(x):
-    sum_terms = sum((4 - 2.1 * x[i] ** 2 + x[i] ** 4 / 3) * x[i] ** 2 + x[i] *
-                    x[i + 1] + (4 * x[i + 1] ** 2 - 4) * x[i + 1] ** 2 for i in range(len(x) - 1))
-    return sum_terms
+# 15. Trid
+def trid(x):
+    x = np.asarray_chkfinite(x)
+    return sum((x - 1) ** 2) - sum(x[:-1] * x[1:])
 
 
 # 16. Drop-wave
 def drop_wave_general(x):
-    sum_terms = sum(- (1 + np.cos(12 * np.sqrt(x[i] ** 2 + x[i + 1] ** 2))) / (
-            0.5 * (x[i] ** 2 + x[i + 1] ** 2) + 2) for i in range(len(x) - 1))
+    sum_terms = sum(
+        -(1 + np.cos(12 * np.sqrt(x[i] ** 2 + x[i + 1] ** 2))) / (0.5 * (x[i] ** 2 + x[i + 1] ** 2) + 2)
+        for i in range(len(x) - 1)
+    )
     return sum_terms
 
 
-# 17. Bukin_n6 ??????????
-def bukin_n6_general(x):
-    sum_terms = sum(100 * np.sqrt(abs(x[i + 1] - 0.01 * x[i] ** 2)) +
-                    0.01 * abs(x[i] + 10) for i in range(len(x) - 1))
-    return sum_terms
+# 17. Perm ###
+def perm(x, b=0.5):
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+    j = np.arange(1.0, n + 1)
+    xbyj = np.fabs(x) / j
+    return mean([mean((j**k + b) * (xbyj**k - 1)) ** 2 for k in j / n])
 
 
-# 18. Goldstein-price ????
-def goldstein_price_general(x):
-    sum_terms = sum((1 + (x[i] + x[i + 1] + 1) ** 2 * (
-            19 - 14 * x[i] + 3 * x[i] ** 2 - 14 * x[i + 1] + 6 * x[i] * x[i + 1] + 3 * x[i + 1] ** 2)) * (30 + (
-            2 * x[i] - 3 * x[i + 1]) ** 2 * (18 - 32 * x[i] + 12 * x[i] ** 2 + 48 * x[i + 1] - 36 * x[i] * x[
-        i + 1] + 27 * x[i + 1] ** 2)) for i in range(len(x) - 1))
-    return sum_terms
+# 18. Michalewicz ####
+def michalewicz(x):  # mich.m
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+    j = np.arange(1.0, n + 1)
+    return -sum(sin(x) * sin(j * x**2 / pi) ** (2 * 0.5))
 
 
-# 19. De Jong f4 ?????
-def de_jong_f4(x):
-    return sum((i + 1) * x_i ** 4 for i, x_i in enumerate(x))
+# 19. Saddle
+def dixonprice(x):  # dp.m
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+    j = np.arange(2, n + 1)
+    x2 = 2 * x**2
+    return sum(j * (x2[1:] - x[:-1]) ** 2) + (x[0] - 1) ** 2
 
 
-# 20. Schaffer n4 #########
-def schaffer_n4(x):
-    return sum(
-        0.5 + (np.sin(np.sqrt(x[i] ** 2 + x[i + 1] ** 2)) ** 2 - 0.5) / (1 + 0.001 * (x[i] ** 2 + x[i + 1] ** 2)) ** 2
-        for i in range(len(x) - 1))
+# 20. Saddle
+def saddle(x):
+    x = np.asarray_chkfinite(x) - 1
+    return np.mean(np.diff(x**2)) + 0.5 * np.mean(x**4)
 
 
-# 21. Salomon ????????
+# 21. Ellipse
+def ellipse( x ):
+    x = np.asarray_chkfinite(x)
+    return mean( (1 - x) **2 )  + 100 * mean( np.diff(x) **2 )
+
+
+# 22. Salomon  
 def salomon(x):
-    r = np.sqrt(sum(i ** 2 for i in x))
+    r = np.sqrt(sum(x**2))
     return 1 - np.cos(2 * np.pi * r) + 0.1 * r
 
 
-# 22. Schaffer n2 ##########
-def schaffer_n2(x):
-    return sum(
-        0.5 + (np.sin(x[i] ** 2 - x[i + 1] ** 2) ** 2 - 0.5) / (1 + 0.001 * (x[i] ** 2 - x[i + 1] ** 2)) ** 2 for i in
-        range(len(x) - 1))
+# 23. Sum of Different Powers 
+def sum_of_different_powers(x):
+    return sum(abs(x_i)**(i+1) for i, x_i in enumerate(x))
 
 
-# 23. Sine-Envelope Sine-Wave ##########
-def sine_envelope_sine_wave(x):
-    return sum(
-        0.5 + (np.sin(np.sqrt(x[i] ** 2 + x[i + 1] ** 2)) ** 2 - 0.5) / (1 + 0.001 * (x[i] ** 2 + x[i + 1] ** 2)) ** 2
-        for i in range(len(x) - 1))
-
-
-# 24. Modified-Exponential ########
-def modified_exponential(x):
-    return -np.exp(-0.5 * sum(i ** 2 for i in x))
+# 24. Alpine 
+def alpine_function(x):
+    return sum(abs(x_i * np.sin(x_i) + 0.1 * x_i) for x_i in x)
 
 
 # 25. Modofied Bent Cigar ########
 def modified_bent_cigar(x):
-    return x[0] ** 2 + 10 ** 6 * sum(i ** 2 for i in x[1:])
+    return x[0] ** 2 + 10**6 * sum(i**2 for i in x[1:])
--- main.py
+++ main.py
@@ -7,9 +7,7 @@ upper_bound = 101
 def generate_population(count, dimension):
     population = []
     for i in range(count):
-        population.append(
-            list(np.random.randint(low=lower_bound, high=upper_bound, size=dimension))
-        )
+        population.append(list(np.random.randint(low=lower_bound, high=upper_bound, size=dimension)))
     return population
 
 
@@ -26,7 +24,6 @@ def de_rand_1_bin(func, dim, pop_size, F=0.8, CR=0.9):
             candidates = list(range(0, i)) + list(range(i + 1, pop_size))
             indexes = np.random.choice(candidates, 3, replace=False)
             r1, r2, r3 = population[indexes[0]], population[indexes[1]], population[indexes[2]]
-
 
             # Mutace
             mutant = r1 + F * (r2 - r3)
@@ -64,19 +61,19 @@ print(de_rand_1_bin(levy, dim_count, pop_count), levy.__name__)
 print(de_rand_1_bin(schwefel, dim_count, pop_count), schwefel.__name__)
 print(de_rand_1_bin(zakharov, dim_count, pop_count), zakharov.__name__)
 print(de_rand_1_bin(styblinski_tang, dim_count, pop_count), styblinski_tang.__name__)
-print(de_rand_1_bin(matyas_general, dim_count, pop_count), matyas_general.__name__)
+print(de_rand_1_bin(powersum, dim_count, pop_count), powersum.__name__)
 print(de_rand_1_bin(booth_general, dim_count, pop_count), booth_general.__name__)
 print(de_rand_1_bin(three_hump_camel_general, dim_count, pop_count), three_hump_camel_general.__name__)
-print(de_rand_1_bin(easom_general, dim_count, pop_count), easom_general.__name__)
-print(de_rand_1_bin(cross_in_tray_general, dim_count, pop_count), cross_in_tray_general.__name__)
-print(de_rand_1_bin(six_hump_camel_back_general, dim_count, pop_count), six_hump_camel_back_general.__name__)
+print(de_rand_1_bin(sum2, dim_count, pop_count), sum2.__name__)
+print(de_rand_1_bin(powell, dim_count, pop_count), powell.__name__)
+print(de_rand_1_bin(trid, dim_count, pop_count), trid.__name__)
 print(de_rand_1_bin(drop_wave_general, dim_count, pop_count), drop_wave_general.__name__)
-print(de_rand_1_bin(bukin_n6_general, dim_count, pop_count), bukin_n6_general.__name__)
-print(de_rand_1_bin(goldstein_price_general, dim_count, pop_count), goldstein_price_general.__name__)
-print(de_rand_1_bin(de_jong_f4, dim_count, pop_count), de_jong_f4.__name__)
-print(de_rand_1_bin(schaffer_n4, dim_count, pop_count), schaffer_n4.__name__)
+print(de_rand_1_bin(perm, dim_count, pop_count), perm.__name__)
+print(de_rand_1_bin(michalewicz, dim_count, pop_count), michalewicz.__name__)
+print(de_rand_1_bin(dixonprice, dim_count, pop_count), dixonprice.__name__)
+print(de_rand_1_bin(saddle, dim_count, pop_count), saddle.__name__)
+print(de_rand_1_bin(ellipse, dim_count, pop_count), ellipse.__name__)
 print(de_rand_1_bin(salomon, dim_count, pop_count), salomon.__name__)
-print(de_rand_1_bin(schaffer_n2, dim_count, pop_count), schaffer_n2.__name__)
-print(de_rand_1_bin(sine_envelope_sine_wave, dim_count, pop_count), sine_envelope_sine_wave.__name__)
-print(de_rand_1_bin(modified_exponential, dim_count, pop_count), modified_exponential.__name__)
+print(de_rand_1_bin(sum_of_different_powers, dim_count, pop_count), sum_of_different_powers.__name__)
+print(de_rand_1_bin(alpine_function, dim_count, pop_count), alpine_function.__name__)
 print(de_rand_1_bin(modified_bent_cigar, dim_count, pop_count), modified_bent_cigar.__name__)
